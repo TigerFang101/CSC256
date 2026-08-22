6716fae88f7416131b95719fbab7ea1c72f294a4
TigerFang101 <zzcat7@gmail.com>
2026-08-21 18:17:40 -0700

    Add files via upload

---
+++ studentinfo.js
@@ -0,0 +1,10 @@
+let name = "Zane Rangel";
+let major = "Advanced Computer Science";
+let email = "zrangel@uat.edu";
+let graduation = "May 2027";
+
+document.getElementById("studentInfo").innerHTML =
+    "Name: " + name + "<br>" +
+    "Major: " + major + "<br>" +
+    "UAT Email: " + email + "<br>" +
+    "Expected Graduation: " + graduation;
